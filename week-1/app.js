86a2841e23179c35e968bc90277490759a08bd0a
peichincc <104899687+peichincc@users.noreply.github.com>
2022-06-17 14:32:09 +0800

    add

---
+++ week-1/app.js
@@ -0,0 +1,9 @@
+// Welcome change
+
+const welcomechange = document.getElementById('welcome');
+
+welcomechange.addEventListener('click', () => {
+  const welcome = document.getElementById('welcome');
+
+  welcomechange.textContent = `Have a nice day.`;
+});

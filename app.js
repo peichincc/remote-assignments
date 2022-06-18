448d120d77af8b5e6a7c75fca568a965adb2a369
peichincc <104899687+peichincc@users.noreply.github.com>
2022-06-18 19:44:51 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// Welcome change
-
-const welcomechange = document.getElementById('welcome');
-
-welcomechange.addEventListener('click', () => {
-  const welcome = document.getElementById('welcome');
-
-  welcomechange.textContent = `Have a nice day.`;
-});

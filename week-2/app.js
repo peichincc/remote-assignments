84232232912b96237a3a8b86e83daa79332dbc29
peichincc <104899687+peichincc@users.noreply.github.com>
2022-06-18 19:33:15 +0800

    add

--- week-2/app.js
+++ week-2/app.js
@@ -21,6 +21,35 @@ btnToggle.addEventListener('click', () => {
   }
 });
 
-// Menu
+// aside Menu
+
+// const BurgerIcon = document.querySelector('label');
+
+// BurgerIcon.addEventListener('click', () => {
+//   const asideMenu = document.querySelector('.aside_menu');
+//   // const mainMenu = document.querySelector('.nav');
+//   // mainMenu.remove();
+
+//   asideMenu.style.display = "block";
+
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
 
 
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
